f06052460828578a76f2e543d5753c4adced4018
DTrung <ngdinhtrung.qt@gmail.com>
2023-09-07 10:29:06 +0700

    1st commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import Head from "next/head";
+
+import "@/styles/globals.scss";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Kinect Portal</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,29 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      fontFamily: {
+        roboto: ["Roboto", "sans-serif"],
+      },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        welcomePage: "url('/images/welcome_background.jpg')",
+        blueBagGradient:
+          "linear-gradient(93.29deg, #4776E6 0%, #4776E6 31.25%, #8E54E9 100%);",
+        blueBorderGradient:
+          "linear-gradient(93.29deg, #4776E6 0%, #FFFFFF 24.48%, #8E54E9 100%)",
+      },
+      colors: {
+        lightGrey: "rgba(35,50,60,0.5)",
+        lightBlue: "rgba(80, 171, 255, 1)",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
